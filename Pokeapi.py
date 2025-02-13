10476a72040793628bea35d786f342dfb2cc3d2b
Emersoncimar <ducho.ribeeiro@gmail.com>
2025-02-13 10:08:47 -0300

    Adicionando novas funções

--- Pokeapi.py
+++ Pokeapi.py
@@ -5,6 +5,18 @@ ALL_JSON = "/api/v2/"
 
 def get_links():
     response = get(BASE_URL+ALL_JSON).json()
-    return list(response.values())
+    return response
 
-print(get_links())
+def get_pokemons():
+    response = get(get_links()['pokemon']).json()
+    return response
+
+def chose_pokemon(name):
+    response = get(get_links()['pokemon'] + name.lower()).json()
+    return response
+
+def pokemon_sprites(name):
+    response = get(get_links()['pokemon'] + name.lower()).json()
+    return response['sprites']['front_default']
+
+print(pokemon_sprites('caterpie'))
